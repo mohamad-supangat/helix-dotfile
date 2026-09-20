5ec16f068075169a4efddca64c2b36b7d2d26bb4
Mohamad Supangat <moha.supangat@gmail.com>
2026-09-20 23:30:09 +0700

    feat: helix plugin is so good

--- init.scm
+++ init.scm
@@ -1,3 +1,6 @@
 ;; Run at startup. Helix context is bound to *helix.cx*
 (require (prefix-in helix. "helix/commands.scm"))
 (require (prefix-in helix.static. "helix/static.scm"))
+
+(require "notify/notify.scm")
+(require "matte.hx/matte.scm")
